f85272b309d84e4176b970b73a2c16861b9619eb
SirSylvester <157227249+SirSylvester@users.noreply.github.com>
2024-09-15 12:50:15 +0300

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,3 +104,60 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+def test_read_a_product(self):
+    """It should Read a Product"""
+    product = ProductFactory()
+    product.id = None
+    product.create()
+    self.assertIsNotNone(product.id)
+    found_product = Product.find(product.id)
+    self.assertEqual(found_product.id, product.id)
+    self.assertEqual(found_product.name, product.name)
+    self.assertEqual(found_product.description, product.description)
+    self.assertEqual(found_product.price, product.price)
+
+def test_delete_a_product(self):
+    """It should Delete a Product"""
+    product = ProductFactory()
+    product.create()
+    self.assertEqual(len(Product.all()), 1)
+    product.delete()
+    self.assertEqual(len(Product.all()), 0)
+
+def test_list_all_products(self):
+    """It should List all Products in the database"""
+    products = Product.all()
+    self.assertEqual(products, [])
+    for _ in range(5):
+        product = ProductFactory()
+        product.create()
+    products = Product.all()
+    self.assertEqual(len(products), 5)
+def test_find_by_name(self):
+    """It should Find a Product by Name"""
+    products = ProductFactory.create_batch(5)
+    for product in products:
+        product.create()
+    name = products[0].name
+    found = Product.find_by_name(name)
+    self.assertEqual(found.count(), 1)
+    self.assertEqual(found[0].name, name)
+def test_find_by_name(self):
+    """It should Find a Product by Name"""
+    products = ProductFactory.create_batch(5)
+    for product in products:
+        product.create()
+    name = products[0].name
+    found = Product.find_by_name(name)
+    self.assertEqual(found.count(), 1)
+    self.assertEqual(found[0].name, name)
+def test_find_by_availability(self):
+    """It should Find Products by Availability"""
+    products = ProductFactory.create_batch(5)
+    for product in products:
+        product.create()
+    available = products[0].available
+    found = Product.find_by_availability(available)
+    self.assertGreaterEqual(len(found), 1)
+    for product in found:
+        self.assertEqual(product.available, available)
